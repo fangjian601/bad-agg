fd144eec5308935729669b84a2ede77daa04ee89
Jian Fang <jfang@rocketfuel.com>
2014-07-26 16:47:47 -0700

    add some files

---
+++ src/com/palantir/hackthon/Util.java
@@ -0,0 +1,17 @@
+package com.palantir.hackthon;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created at 7/26/14
+ *
+ * @author Jian Fang (jfang@rocketfuelinc.com)
+ */
+public class Util {
+    public static Map<String, List<DataItem>> readFromOffsets(String fileName, long beginOffset, long endOffset)
+            throws IOException{
+        return null;
+    }
+}
