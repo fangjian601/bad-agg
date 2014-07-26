855a7978fd918b85d7d44b8e44ef1e19e880f0d0
Jian Fang <jfang@rocketfuel.com>
2014-07-26 16:43:38 -0700

    add some empty files

---
+++ src/com/palantir/hackthon/QueryWorker.java
@@ -0,0 +1,18 @@
+package com.palantir.hackthon;
+
+/**
+ * Created at 7/26/14
+ *
+ * @author Jian Fang (jfang@rocketfuelinc.com)
+ */
+public class QueryWorker implements Runnable {
+
+    public QueryWorker(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
