174ca9fd545c7094323cb7ef4898d2fe902ef695
Jian Fang <jfang@rocketfuel.com>
2014-07-26 18:40:37 -0700

    git add badagg

---
+++ src/com/palantir/hackthon/BadAgg.java
@@ -0,0 +1,32 @@
+package com.palantir.hackthon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class BadAgg {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            QueryCoordinator coordinator = new QueryCoordinator(32, sc.nextLine());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String [] command = br.readLine().split(" ");
+                if (command[0].equalsIgnoreCase("average")) {
+                    System.out.println(coordinator.average(command[1]));
+                } else if (command[0].equalsIgnoreCase("top10")) {
+
+                } else if (command[0].equalsIgnoreCase("rangemax")) {
+                    System.out.println(coordinator.rangeMax(command[1], command[2]));
+                } else {
+                    return;
+                }
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
+
