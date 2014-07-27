888d0fcf68a5c8190a1ba61d28952b3e1b8b90f4
Jian Fang <jfang@rocketfuel.com>
2014-07-26 18:10:09 -0700

    add several things

--- src/com/palantir/hackthon/QueryHelper.java
+++ src/com/palantir/hackthon/QueryHelper.java
@@ -1,6 +1,9 @@
 package com.palantir.hackthon;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -14,5 +17,44 @@ public class QueryHelper {
             throws IOException{
         return null;
     }
-    
+
+    public static List<Long> splitFile(String fileName, int num){
+        List<Long> result = new ArrayList<Long>();
+        File file = new File(fileName);
+        long fileSize = file.length();
+        if(fileSize <= num){
+            result.add(0l);
+        } else {
+            long size = fileSize / num;
+            for(long offset = 0; offset < fileSize; offset += size){
+                result.add(offset);
+            }
+        }
+        result.add(fileSize - 1);
+        return result;
+    }
+
+    public static List<Long> averageQuery(Map<String, List<DataItem>> data, String prefix){
+        Long[] result = new Long[100];
+        for (Map.Entry<String, List<DataItem>> entry : data.entrySet()){
+            for(DataItem item : entry.getValue()){
+                if(item.getName().startsWith(prefix) && item.getAge() >=0 && item.getAge() < 100){
+                    result[item.getAge()] = result[item.getAge()] + 1;
+                }
+            }
+        }
+        return Arrays.asList(result);
+    }
+
+    public static int averageQueryAggregate(List<List<Long>> agesCounts){
+        long sum = 0, count = 0;
+        for(List<Long> agesCount : agesCounts){
+            for(int i = 0; i < agesCount.size(); i++){
+                count += agesCount.get(i);
+                sum += (i * agesCount.get(i));
+            }
+        }
+        return (int)(sum / count);
+    }
+
 }
