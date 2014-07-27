4f8b76a3b0b24f6e611f5b6090ce6721a04d2572
Jian Fang <jfang@rocketfuel.com>
2014-07-26 18:17:12 -0700

    merge with micheal

--- src/com/palantir/hackthon/QueryHelper.java
+++ src/com/palantir/hackthon/QueryHelper.java
@@ -2,13 +2,9 @@ package com.palantir.hackthon;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created at 7/26/14
@@ -18,7 +14,66 @@ import java.util.Map;
 public class QueryHelper {
 	
 	static final String DELIM = ",";
-	
+
+    private static int bsearchLeft(int age, List<DataItem> l){
+        if (age < 0) {
+            return -1;
+        }
+
+        int lo = 0;
+        int hi = l.size() - 1;
+        int mid = 0;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            mid = lo + ((hi - lo) >>> 1);
+            if      (age < l.get(mid).getAge()) hi = mid - 1;
+            else if (age > l.get(mid).getAge()) lo = mid + 1;
+            else break;
+        }
+
+        int index = mid;
+        while (index >= 0 && age == l.get(index).getAge()) {
+            index--;
+        }
+        return index + 1;
+    }
+
+    private static int bsearchRight(int age, List<DataItem> l){
+        if (age < 0) {
+            return -1;
+        }
+
+        int lo = 0;
+        int hi = l.size() - 1;
+        int mid = 0;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            mid = lo + ((hi - lo) >>> 1);
+            if      (age < l.get(mid).getAge()) hi = mid - 1;
+            else if (age > l.get(mid).getAge()) lo = mid + 1;
+            else break;
+        }
+
+        int index = mid;
+        while (index < l.size() && age == l.get(index).getAge()) {
+            index++;
+        }
+        return index - 1;
+    }
+
+    public static Map<String, Integer> precomputedComputeRangeMax(Map<String, List<DataItem>> data, int startAge, int endAge) {
+        Map<String,Integer> countsByState = new HashMap<String, Integer>();
+        Iterator<Map.Entry<String, List<DataItem>>> it = data.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, List<DataItem>> stateData = it.next();
+            int left = bsearchLeft(startAge, stateData.getValue());
+            int right = bsearchRight(endAge, stateData.getValue());
+            countsByState.put(stateData.getKey(), right-left + 1);
+        }
+
+        return countsByState;
+    }
+
     public static Map<String, List<DataItem>> readFromOffsets(String fileName, long beginOffset, long endOffset){
 
     	Map<String, List<DataItem>> resultMap = new HashMap<String, List<DataItem>>();
@@ -107,4 +162,24 @@ public class QueryHelper {
         }
         return (int)(sum / count);
     }
+
+    public static Map<String, Integer> rangeMaxQuery(List<List<String>> data, int startAge, int endAge) {
+        Map<String,Integer> countsByState = new HashMap<String, Integer>();
+        for (List<String> row : data) {
+            int age = Integer.parseInt(row.get(1));
+            String state = row.get(2);
+            if (age >= startAge && age <= endAge) {
+                Integer count = countsByState.get(state);
+                if (count != null) {
+                    countsByState.put(state, count + 1);
+                } else {
+                    countsByState.put(state, 1);
+                }
+            }
+        }
+
+        return countsByState;
+    }
+
+
 }
